4ed83da3b0491490cbb1b762c893bf13aefdd85c
Zhenya <ruban.z1@mail.ru>
2022-06-07 22:30:13 +0300

    Функция фильтрации найденных товаров

--- js/main.js
+++ js/main.js
@@ -1,43 +1,56 @@
 
-const results = document.getElementById('results');
+const domElements = {
+    results: document.getElementById('results'),
+    search: {
+        input: document.getElementById('search-input'),
+        button: document.getElementById('search-button')
+    }
+};
 
 
 
-function generateCards(count) {
+function generateCards(data) {
     const cards = [];
-    for(let i = 0; i < count; i++) {
+    for(let i = 0; i < data.length; i++) {
+        let countClass = 'card__count';
+        let countValue = data[i].count;
+
+        if(data[i].count === 0) {
+            countClass = 'card__count card__count_empty';
+            countValue = 'Нет в наличии';
+        }
         cards.push(`
             <div class="card">
             <img src="https://placeimg.com/300/200/any?id=${i}" alt="" class="card__img">
             <div class="card__content">
-                <h3 class="card__title">Название товара</h3>
-                <div class="card__description">Описание товара</div>
+                <h3 class="card__title">${data[i].title}</h3>
+                <div class="card__description">${data[i].description}</div>
                 <div class="card__info">
                     <div class="card__param">
                         <label>Год:</label>
-                        <div id="year">2022</div>
+                        <div id="year">${data[i].params.year}</div>
                     </div>
                     <div class="card__param">
                         <label>Цвет:</label>
-                        <div id="color">Красный</div>
+                        <div id="color">${data[i].params.color}</div>
                     </div>
                     <div class="card__param">
                         <label>Страна:</label>
-                        <div id="country">Россия</div>
+                        <div id="country">${data[i].params.country}</div>
                     </div>
                     <div class="card__param">
                         <label>Категория:</label>
-                        <div id="color">Техника</div>
+                        <div id="color">${data[i].params.category}</div>
                     </div>    
                 </div>
                 <div class="card__footer">
-                    <div class="card__count">
+                    <div class="${countClass}">
                         <label>Количество:</label>
-                        <div id="count">3</div>
+                        <div id="count">${countValue}</div>
                     </div>
                     <div class="card__cost">
                         <label>Цена:</label>
-                        <div>300</div>
+                        <div>${data[i].cost}</div>
                     </div>
                 </div>
             </div>
@@ -47,6 +60,38 @@ function generateCards(count) {
     return cards;
 }
 
-const cardsArr = generateCards(5);
+//Генерация карточек
+const cardsArr = generateCards(cardsData);
+domElements.results.innerHTML = cardsArr.join('');
 
-results.innerHTML = cardsArr.join('');
+
+//Поиск товаров
+
+
+let searchValue = '';
+// Изменение значения поля поиска
+domElements.search.input.oninput = (event) => {
+    const target = event.target;
+    searchValue = target.value;
+};
+
+//Клик по кнопке поиска
+domElements.search.button.onclick = () => {
+    filterSearch();
+};
+
+
+//Функция фильтрации найденых товаров
+function filterSearch() {
+  const rgx = new RegExp(searchValue, 'i');
+  let filteredCardsData = cardsData.filter(card => {
+        if(rgx.test(card.title)) {
+            return true;
+        } else {
+            return false;
+        }
+    }); 
+    const newFilteredCardsHTML = generateCards(filteredCardsData);
+
+    domElements.results.innerHTML = newFilteredCardsHTML.join('');
+}
